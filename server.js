357ba49c7610b07ff933e5401825f875a22fcb39
koodoshujin <flo4biz@gmail.com>
2023-01-13 08:53:16 +0100

    debugging

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(bodyParser.json());
 const port = process.env.PORT || config.PORT;
 
 
-app.use("/api/v1/product",productRoutes)
+app.get("/", (req, res)=> {
+    res.send("we are live on server");
+})
+
+// app.use("/api/v1/product",productRoutes)
 
 app.listen(port, (req, res)=>{
     console.log("we are live on port" + port);
