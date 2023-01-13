1a815379cef762759ee5b569479fbed1a4d04e6a
koodoshujin <flo4biz@gmail.com>
2023-01-13 09:11:36 +0100

    check

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 require('dotenv').config();
+var cors = require('cors');
 const productRoutes = require("./src/product/routes");
 const config = require('./config')
 
 const app = express();
 app.use(bodyParser.json()); 
+app.use(cors());
 
 const port = process.env.PORT || config.PORT;
 
